3a9c4d6f0a051e588fe233b1e1483b0a74ee39b7
rjm0999 <103060469+rjm0999@users.noreply.github.com>
2022-07-06 21:23:33 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,8 +1,9 @@
 import tkinter as tk
-# import pandas as pd
+import pandas as pd
+from datetime import date
 
-
-FONT = "Comic Sans MS"
+from constants import FONT
+from constants import DEPARTMENTS
 
 
 class Gui(tk.Frame):
@@ -11,25 +12,40 @@ class Gui(tk.Frame):
         self.parent = parent
         self.destroy_children()
 
-    # destroys all widgets that have the same parent as self, excluding self (alternate name - fratricide)
     def destroy_children(self):
+        """
+        Retrieve the parent of this widget and call the destroy method on all children of the parent, excluding itself
+        """
         for i in self.parent.winfo_children():
             if i is not self:
                 i.destroy()
 
-    # base function to configure the parent widget in preparation for drawing the gui
     def configure_parent(self):
+        """
+        Call the clear_grid method on the parent object of this widget. Acts as default configure_parent for all
+        classes that inherit from this one
+        """
         self.parent.clear_grid()
 
-    # creates main menu and quit buttons for this window
     def frame_buttons(self):
+        """
+        Create two buttons, one that closes the program and one that returns to the main menu, and pack them into a
+        frame
+
+        Returns
+        -------
+        Frm : tkinter.Frame
+            Frame containing the "Main Menu" and "Quit" buttons
+        """
         frm = tk.Frame(self.parent)
 
         back_button = tk.Button(frm, text="Main Menu", command=self.parent.main_gui, font=(FONT, 25))
         back_button.pack(expand=True, fill=tk.BOTH, side=tk.LEFT, padx=50, pady=50)
+        back_button.config(width=1)
 
         quit_button = tk.Button(frm, text="Quit", command=self.quit, font=(FONT, 25))
         quit_button.pack(expand=True, fill=tk.BOTH, side=tk.RIGHT, padx=50, pady=50)
+        quit_button.config(width=1)
 
         return frm
 
@@ -43,30 +59,47 @@ class MainGui(Gui):
 
         self.buttons()
 
-    # creates the buttons for inventory, orders, requisitions, audits, and quitting the program
     def buttons(self):
-        tk.Button(self, text="Quit", command=self.quit, font=(FONT, 25)) \
-            .grid(row=3, column=0, columnspan=2, sticky=tk.NSEW, padx=50, pady=50)
-        tk.Button(self, text="Current Inventory", command=lambda: self.parent.inv_gui(), font=(FONT, 25)) \
-            .grid(row=0, column=0, sticky=tk.NSEW, padx=50, pady=50)
-        tk.Button(self, text="Requisitions", command=lambda: self.parent.req_gui(), font=(FONT, 25)) \
-            .grid(row=0, column=1, sticky=tk.NSEW, padx=50, pady=50)
-        tk.Button(self, text="Audit", command=lambda: self.parent.audit_gui(), font=(FONT, 25)) \
-            .grid(row=1, column=0, sticky=tk.NSEW, padx=50, pady=50)
-        tk.Button(self, text="Deliveries", command=lambda: self.parent.delivery_gui(), font=(FONT, 25)) \
-            .grid(row=1, column=1, sticky=tk.NSEW, padx=50, pady=50)
-        tk.Button(self, text="Orders", command=lambda: self.parent.order_gui(), font=(FONT, 25)) \
-            .grid(row=2, column=0, sticky=tk.NSEW, padx=50, pady=50)
+        """
+        Create the buttons that open each gui of the program and pack them into a grid
+        """
+        cur_inv = tk.Button(self, text="Current Inventory", command=self.parent.inv_gui, font=(FONT, 25))
+        cur_inv.grid(row=0, column=0, sticky=tk.NSEW, padx=50, pady=50)
+        cur_inv.config(width=1)
 
-    # configures the grid of the parent widget for this frame
+        req = tk.Button(self, text="Requisitions", command=self.parent.req_gui, font=(FONT, 25))
+        req.grid(row=0, column=1, sticky=tk.NSEW, padx=50, pady=50)
+        req.config(width=1)
+
+        audit = tk.Button(self, text="Audit", command=self.parent.audit_gui, font=(FONT, 25))
+        audit.grid(row=1, column=0, sticky=tk.NSEW, padx=50, pady=50)
+        audit.config(width=1)
+
+        deliveries = tk.Button(self, text="Deliveries", command=self.parent.delivery_gui, font=(FONT, 25))
+        deliveries.grid(row=1, column=1, sticky=tk.NSEW, padx=50, pady=50)
+        deliveries.config(width=1)
+
+        orders = tk.Button(self, text="Orders", command=self.parent.order_gui, font=(FONT, 25))
+        orders.grid(row=2, column=0, sticky=tk.NSEW, padx=50, pady=50)
+        orders.config(width=1)
+
+        quit_button = tk.Button(self, text="Quit", command=self.quit, font=(FONT, 25))
+        quit_button.grid(row=3, column=0, columnspan=2, sticky=tk.NSEW, padx=50, pady=50)
+        quit_button.config(width=1)
+
     def configure_parent(self):
+        """
+        Configure the grid of the parent widget for this frame
+        """
         super().configure_parent()
 
         self.parent.columnconfigure(0, weight=1)
         self.parent.rowconfigure(0, weight=1)
 
-    # configures grid for this frame
     def configure_self(self):
+        """
+        Configure the grid of this frame
+        """
         self.grid(sticky=tk.NSEW)
 
         self.columnconfigure(0, weight=1)
@@ -84,8 +117,8 @@ class InventoryGui(Gui):
         self.item_quantity = tk.StringVar(self)
         self.item_quantity_label = None
 
-        self.on_order = tk.StringVar(self)
-        self.on_order_label = None
+        self.awaiting_delivery = tk.StringVar(self)
+        self.awaiting_delivery_label = None
 
         self.configure_parent()
         self.configure_self()
@@ -97,8 +130,10 @@ class InventoryGui(Gui):
 
         # TODO Inventory gui widgets
 
-    # configure root window for inventory gui
     def configure_parent(self):
+        """
+        Configure the parent widget's grid for this gui
+        """
         super().configure_parent()
  
         self.parent.columnconfigure(0, weight=1)
@@ -108,26 +143,32 @@ class InventoryGui(Gui):
         self.parent.rowconfigure(1, weight=1)
         # TODO configure root for inventory gui frame
 
-    # configure grid on inventory gui
     def configure_self(self):
+        """
+        Configure the grid for this widget
+        """
         self.grid(row=0, column=0, sticky=tk.NSEW)
 
-        self.columnconfigure(0, weight=19)
+        self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=1)
 
         self.rowconfigure(0, weight=1)
-        # TODO configure inventory gui frame - figure out why right side of screen is so much larger than left
 
-    # creates the listbox for the inventory gui
     def create_listbox(self):
+        """
+        Create a tkinter.Listbox widget for the inventory gui
+        """
         lb = tk.Listbox(self, listvariable=tk.StringVar(value=self.parent.inv["Name"].values.tolist()),
-                        font=(FONT, 20))
+                        font=(FONT, 25))
         lb.grid(row=0, column=0, sticky=tk.NSEW, padx=50, pady=50)
+        lb.config(width=0)
 
         lb.bind("<<ListboxSelect>>", lambda e: self.listbox_change(lb))
 
-    # creates a frame widget to hold the label widgets
     def frame_entry(self):
+        """
+        Create and configure the grid of a tkinter.Frame widget to hold tkinter.Label widgets
+        """
         frame_entry = tk.Frame(self)
 
         frame_entry.grid(row=0, column=1, sticky=tk.NSEW)
@@ -135,33 +176,39 @@ class InventoryGui(Gui):
         frame_entry.rowconfigure(0, weight=1)
         frame_entry.rowconfigure(1, weight=1)
         frame_entry.rowconfigure(2, weight=1)
-        frame_entry.rowconfigure(3, weight=1)
 
         frame_entry.columnconfigure(0, weight=1)
 
         self.item_quantity_label = tk.Label(frame_entry, text="Current Inventory: ", font=(FONT, 25))
-        self.item_quantity_label.grid(row=0, column=0, sticky=tk.NSEW)
+        self.item_quantity_label.grid(row=0, column=0, sticky=tk.NSEW, padx=50, pady=50)
+        self.item_quantity_label.config(width=1)
 
-        self.frame_on_order(frame_entry)
+        self.awaiting_delivery_label = tk.Label(frame_entry, text="Awaiting Delivery: ", font=(FONT, 25))
+        self.awaiting_delivery_label.grid(row=1, column=0, sticky=tk.NSEW, padx=50, pady=50)
+        self.awaiting_delivery_label.config(width=1)
 
-    # create a frame to hold the order label and order buttons
-    def frame_on_order(self, frame):
-        frame_oo = tk.Frame(frame)
-        frame_oo.grid(row=1, column=0, sticky=tk.NSEW)
+        deliveries = tk.Button(frame_entry, text="Go to Deliveries", command=self.parent.delivery_gui, font=(FONT, 25))
+        deliveries.grid(row=2, column=0, sticky=tk.NSEW, padx=50, pady=50)
 
-        self.on_order_label = tk.Label(frame_oo, text="On Order: ", font=(FONT, 25))
-        self.on_order_label.pack(expand=True, side=tk.LEFT, padx=50, pady=50)
-
-        tk.Button(frame_oo, text="Go to Orders", command=self.parent.order_gui, font=(FONT, 25))\
-            .pack(expand=True, side=tk.LEFT, padx=50, pady=50)
-
-    # define behavior for when listbox selection changes
     def listbox_change(self, lb):
-        self.item_quantity.set(self.parent.inv["Quantity"][list(self.parent.inv.index)[lb.curselection()[0]]])
+        """
+        Define behavior for when listbox selection changes
+
+        Parameters
+        ----------
+        lb : tkinter.Listbox
+            the listbox widget to define behavior for
+        """
+        self.item_quantity.set(self.parent.inv["Quantity"][self.parent.inv.index.tolist()[lb.curselection()[0]]])
         self.item_quantity_label.configure(text="Current Inventory: "+self.item_quantity.get())
 
-        self.on_order.set(self.parent.inv["On Order"][lb.curselection()[0]])
-        self.on_order_label.configure(text="On Order: " + self.on_order.get())
+        if self.parent.inv.index.tolist()[lb.curselection()[0]] in self.parent.deliveries.index.tolist():
+            self.awaiting_delivery.set(self.parent.deliveries["Quantity Ordered"]
+                                       [self.parent.inv.index.tolist()[lb.curselection()[0]]])
+        else:
+            self.awaiting_delivery.set("0")
+
+        self.awaiting_delivery_label.configure(text="Awaiting Delivery: " + self.awaiting_delivery.get())
 
     # TODO add an entry widget to search listbox (only show items that contain the string in the entry box?)
     #  will also need to change how indexing in the dataframe is done if this is implemented - low priority
@@ -176,57 +223,104 @@ class ReqGui(Gui):
         super().__init__(parent)
 
         self.req_list = []
+        self.department = tk.StringVar()
+        self.entry_parent = None
 
         self.configure_parent()
         self.configure_self()
 
-        self.frm_req = self.frame_req()
+        self.buttons()
+        self.frame_canvas()
+
         self.frame_buttons().grid(row=1, column=0, sticky=tk.NSEW)
         # TODO reqs gui widgets
 
     def configure_parent(self):
+        """
+        Configure the grid of the parent widget for this frame
+        """
         super().configure_parent()
 
         self.parent.columnconfigure(0, weight=1)
-        self.parent.rowconfigure(0, weight=1)
+
+        self.parent.rowconfigure(0, weight=14)
+        self.parent.rowconfigure(1, weight=1)
         # TODO configure root for req gui frame
 
     def configure_self(self):
-        self.columnconfigure(0, weight=1)
+        """
+        Configure the grid of this frame
+        """
+        self.grid(row=0, column=0, sticky=tk.NSEW)
+
+        self.columnconfigure(0, weight=2)
+        self.columnconfigure(1, weight=1)
+
         self.rowconfigure(0, weight=1)
         # TODO configure req gui frame
 
-    def frame_req(self):
-        frm = tk.Frame(self)
-        frm.grid(row=0, column=0, sticky=tk.NSEW)
+    def buttons(self):
+        """
+        Create the buttons for the right side of the requisitions gui
+        """
+        frame_buttons = tk.Frame(self)
 
-        canvas = tk.Canvas()  # parent of entries
+        frame_buttons.grid(row=0, column=1, sticky=tk.NSEW)
 
-        tk.Button(frm, text="Commit").grid(row=2, column=0, sticky=tk.NSEW)  # apply button - bottom row
-        tk.Button(frm, text="Add row", command=lambda e: self.add_entries(canvas)).grid(row=2, column=1, sticky=tk.NSEW)  # add row - bottom row
-        tk.Entry(frm).grid(row=0, column=0, sticky=tk.NSEW)  # date - top row
+        frame_buttons.rowconfigure(0, weight=1)
+        frame_buttons.columnconfigure(0, weight=1)
+        frame_buttons.columnconfigure(1, weight=1)
+        frame_buttons.columnconfigure(2, weight=1)
+        frame_buttons.columnconfigure(3, weight=1)
 
-        clicked = tk.StringVar()
-        options = [1, 2, 3, 4]
-        tk.OptionMenu(frm, clicked, *options).grid(row=0, column=1, sticky=tk.NSEW)  # department - top row
+        tk.Label(frame_buttons, text=date.today().isoformat(), font=(FONT, 25))\
+            .grid(row=0, column=0, sticky=tk.NSEW, padx=25, pady=25)
+        self.dropdown(frame_buttons)
+        tk.Button(frame_buttons, text="Commit", font=(FONT, 25))\
+            .grid(row=2, column=0, sticky=tk.NSEW, padx=25, pady=25)
+        tk.Button(frame_buttons, text="Add Row", font=(FONT, 25))\
+            .grid(row=3, column=0, sticky=tk.NSEW, padx=25, pady=25)
+        tk.Button(frame_buttons, text="History", font=(FONT, 25))\
+            .grid(row=4, column=0, sticky=tk.NSEW, padx=25, pady=25)
 
-        # TODO finish these widgets have a scrollbar
+    def dropdown(self, frm):
+        """
+        Create dropdown selection for departments for requisitions gui
 
-        return frm
+        Parameters
+        ----------
+        frm : the parent frame to contain the dropdown
+        """
+        dept_list = []
+        for i in DEPARTMENTS.index.tolist():
+            dept_list.append(str(i) + " - " + DEPARTMENTS["Dept"][i])
 
-    def edit_inventory(self):
-        pass
-        # TODO update inventory dataframe, add req to history
+        dd = tk.OptionMenu(frm, self.department, *dept_list)
+        dd.config(font=(FONT, 25), width=15)
+        dd.grid(row=1, column=0, sticky=tk.NSEW, padx=25, pady=25)
 
-    def add_entries(self, canvas):
-        frm = tk.Frame(canvas)
-        frm.pack(fill=tk.X, expand=True, side=tk.TOP)
+    def frame_canvas(self):
+        """
+        Create canvas to hold entry fields for requisitions gui
+        """
+        frame = tk.Frame(self)
+        self.entry_parent = tk.Frame(frame)
+        frame.grid(row=0, column=0, rowspan=5, sticky=tk.NSEW)
 
-        tk.Entry(frm).grid(row=0, column=0, sticky=tk.NSEW)  # quantity
-        tk.Entry(frm).grid(row=0, column=1, sticky=tk.NSEW)  # item # (maybe make this a dropdown?)
+        canvas = tk.Canvas(frame)
+        scroll = tk.Scrollbar(frame, orient=tk.VERTICAL, command=canvas.yview)
 
-        self.req_list.append(["item #", "# of items"])  # TODO finish this
-        pass
+        canvas.create_window(0, 0, anchor=tk.NW, window=self.entry_parent)
+        canvas.configure(scrollregion=canvas.bbox("all"), yscrollcommand=scroll.set)
+
+        canvas.pack(expand=True, fill=tk.BOTH, side=tk.LEFT)
+        scroll.pack(fill=tk.Y, side=tk.RIGHT)
+
+        self.add_row()
+
+    def add_row(self):
+        frm = tk.Frame(self.entry_parent)
+
     # TODO have one csv with the list of dates, use that to get the file name for the corresponding req
     #      use something similar for orders/audits?
 
@@ -245,6 +339,9 @@ class AuditGui(Gui):
         self.frame_buttons().grid(row=1, column=0, sticky=tk.NSEW)
 
     def configure_parent(self):
+        """
+        Configure the grid of the parent widget for this frame
+        """
         super().configure_parent()
 
         self.parent.columnconfigure(0, weight=1)
@@ -253,6 +350,9 @@ class AuditGui(Gui):
         self.parent.rowconfigure(1, weight=1)
 
     def configure_self(self):
+        """
+        Configure the grid of this frame
+        """
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
 
@@ -298,10 +398,10 @@ class AuditGui(Gui):
         wid3 = tk.Entry(top, font=(FONT, 25))
         wid4 = tk.Label(top, textvariable=text_var, font=(FONT, 25))
 
-        wid1.pack(expand=True, fill=tk.BOTH, side=tk.LEFT, padx=25, pady=5)
-        wid2.pack(expand=True, fill=tk.BOTH, side=tk.LEFT, padx=25, pady=5)
-        wid3.pack(expand=True, fill=tk.BOTH, side=tk.LEFT, padx=25, pady=5)
-        wid4.pack(expand=True, fill=tk.BOTH, side=tk.LEFT, padx=25, pady=5)
+        wid1.pack(expand=True, fill=tk.BOTH, side=tk.LEFT, padx=10, pady=5)
+        wid2.pack(expand=True, fill=tk.BOTH, side=tk.LEFT, padx=10, pady=5)
+        wid3.pack(expand=True, fill=tk.BOTH, side=tk.LEFT, padx=10, pady=5)
+        wid4.pack(expand=True, fill=tk.BOTH, side=tk.LEFT, padx=10, pady=5)
 
         return [wid1, wid2, wid3, text_var]
 
@@ -315,9 +415,14 @@ class AuditGui(Gui):
 class OrderGui(Gui):
     def __init__(self, parent):
         super().__init__(parent)
+
+        self.frame_buttons().grid(row=1, column=0, columnspan=2, sticky=tk.NSEW)
         # TODO order widgets
 
     def configure_parent(self):
+        """
+        Configure the grid of the parent widget for this frame
+        """
         super().configure_parent()
 
         self.parent.columnconfigure(0, weight=1)
@@ -325,6 +430,9 @@ class OrderGui(Gui):
         # TODO configure root for order gui frame
 
     def configure_self(self):
+        """
+        Configure the grid of this frame
+        """
         self.grid(row=0, column=0, sticky=tk.NSEW)
 
         self.columnconfigure(0, weight=1)
@@ -344,12 +452,18 @@ class DeliveryGui(Gui):
         self.frame_buttons().grid(row=1, column=0, sticky=tk.NSEW)
 
     def configure_parent(self):
+        """
+        Configure the grid of the parent widget for this frame
+        """
         super().configure_parent()
 
         self.parent.columnconfigure(0, weight=1)
         self.parent.rowconfigure(0, weight=1)
 
     def configure_self(self):
+        """
+        Configure the grid of this frame
+        """
         self.grid(row=0, column=0, sticky=tk.NSEW)
 
         self.columnconfigure(0, weight=1)
@@ -359,7 +473,7 @@ class DeliveryGui(Gui):
 
     def create_listbox(self):
         list_var = []
-        keys = list(self.parent.deliveries.index)
+        keys = self.parent.deliveries.index.tolist()
         idx = 0
         while idx <= self.parent.deliveries.shape[0] - 1:
             key = keys[idx]
